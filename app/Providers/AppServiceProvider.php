0d5352d81ca2b3cbb6d53bc05fac32614d6a9a38
fiqgant <fiqgant@gmail.com>
2026-06-02 08:06:25 +0700

    Force HTTPS scheme in production to fix login form insecure submission warning

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Models\SystemSetting;
 use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -15,6 +16,10 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
+
         $this->loadMailSettingsFromDb();
     }
 
